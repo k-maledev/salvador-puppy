96fd2d189bf7e4b1159ff58d520a00f1052607e3
JAEWOON PARK <kmaledev@gmail.com>
2023-02-11 05:13:23 +0900

    feat: Add link buttons in home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,27 @@
+import { Link } from "react-router-dom";
 import { Overview, Samples, Description, AdoptAnimation } from "../component";
 import styles from "../style";
 
 const Home = () => {
   return (
     <div
-      className={`${styles.container} ${styles.columnCenter} sm:gap-y-48 gap-y-32 sm:mt-32 mt-16`}
+      className={`${styles.container} ${styles.columnCenter} sm:gap-y-48 gap-y-32 sm:mt-32 mt-16 sm:mb-40 mb-24`}
     >
       <Overview />
       <Samples />
       <Description />
       <AdoptAnimation />
+
+      <div
+        className={`xs:flex-row xs:justify-center ${styles.columnCenter} gap-4 xs:w-full w-[80%]`}
+      >
+        <Link to="/adopt" className={styles.buttonWhite}>
+          입양하기
+        </Link>
+        <Link to="/reviews" className={styles.buttonOutlinedWhite}>
+          리뷰 보기
+        </Link>
+      </div>
     </div>
   );
 };
--- src/style.ts
+++ src/style.ts
@@ -3,6 +3,10 @@ const styles = {
   absoluteCenter: "absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2",
   flexCenter: "flex justify-center items-center",
   columnCenter: "flex flex-col items-center",
+  buttonWhite:
+    "xs:w-auto w-full xs:text-xl text-lg xs:px-10 px-4 xs:py-3 py-2 rounded-lg text-center font-medium tracking-widest drop-shadow-lg hover:drop-shadow-xl cursor-pointer hover:translate-y-[-4%] active:translate-y-[-2%] transition-all duration-100 bg-white text-[#333]",
+  buttonOutlinedWhite:
+    "xs:w-auto w-full xs:text-xl text-lg xs:px-10 px-4 xs:py-3 py-2 rounded-lg text-center font-medium tracking-widest drop-shadow-lg hover:drop-shadow-xl cursor-pointer hover:translate-y-[-4%] active:translate-y-[-2%] transition-all duration-100 border-2 border-[#f0f0f0] text-[#f0f0f0]",
 };
 
 export default styles;
