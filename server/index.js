9041dcc6975d4c6d8c81278de8fd8cdbeeb73d01
JAEWOON PARK <kmaledev@gmail.com>
2023-02-13 06:34:59 +0900

    chore: Specify client url for cors

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "https://salvador-puppy.web.app", credentials: true }));
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/dalle", dalleRoutes);
