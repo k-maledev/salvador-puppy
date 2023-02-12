32dc16a00a0324f2f0778f53c1755aa660e2d058
JAEWOON PARK <kmaledev@gmail.com>
2023-02-12 20:21:25 +0900

    feat: Scroll up on every route change

--- client/src/layout/PageLayout.tsx
+++ client/src/layout/PageLayout.tsx
@@ -1,9 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 
 import Header from "./Header";
 import Footer from "./Footer";
 
 const PageLayout = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
     <>
       <Header />
