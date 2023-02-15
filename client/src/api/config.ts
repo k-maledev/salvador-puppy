3e379091606b4afa3a5661ed3442f47f742e27fc
JAEWOON PARK <kmaledev@gmail.com>
2023-02-15 11:15:44 +0900

    chore: Change server url

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -1,2 +1,1 @@
-export const SERVER_URL = "https://localhost:8080";
-// export const SERVER_URL = "https://salvador-puppy.onrender.com";
+export const SERVER_URL = "https://salvador-puppy.onrender.com";
